c04bb2809cdd9e68e7c302bb5ca03b15fc19532f
NicolayBarreno2134 <nicolay.barreno@epn.edu.ec>
2026-04-28 19:08:35 -0500

    Se agrego codigo al Docente el cual heredo lo de la clase Padre

--- src/Ejercicio2/Docente.java
+++ src/Ejercicio2/Docente.java
@@ -2,4 +2,55 @@ package Ejercicio2;
 
 public class Docente extends PersonaAcademica{
 
+    private int horasClase;
+    private double valorHora;
+
+    public void setHorasClase(int horasClase) {
+
+        if (horasClase >= 1 && horasClase <= 40) {
+            this.horasClase = horasClase;
+        } else {
+            System.out.println("Horas inválidas. Se asignara 1.");
+            this.horasClase = 1;
+        }
+    }
+    public void setValorHora(double valorHora) {
+        if (valorHora > 0) {
+            this.valorHora = valorHora;
+        } else {
+            System.out.println("Valor hora inválido. Se asignara 1.");
+            this.valorHora = 1;
+        }
+    }
+
+    public int getHorasClase() {
+        return horasClase;
+    }
+    public double getValorHora() {
+        return valorHora;
+    }
+
+    @Override
+    public double calcularPago() {
+        return horasClase * valorHora;
+    }
+
+    @Override
+    public double calcularPago(double bonoExtra) {
+        return calcularPago() + bonoExtra;
+    }
+
+    @Override
+    public void describirRol() {
+        System.out.println("Rol: Docente.");
+    }
+
+    @Override
+    public void mostrarDatos() {
+        System.out.println("Código: " + getCodigo());
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Edad: " + getEdad());
+        System.out.println("Horas de clase: " + horasClase);
+        System.out.println("Valor por hora: " + valorHora);
+    }
 }
